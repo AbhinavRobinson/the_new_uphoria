9036fa4387e78c58c79208d3eaadb8c286f10f4e
Abhinav Robinson <32090909+AbhinavRobinson@users.noreply.github.com>
2019-01-24 11:17:40 +0530

    Update v0.5 : HERO BALL

--- app/js/main.babel.js
+++ app/js/main.babel.js
@@ -1,6 +1,6 @@
 "use strict";
 
 var script = document.createElement('script');
-script.src = './jquery-3.3.1.min.js';
+script.src = './src/js/jquery-3.3.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
