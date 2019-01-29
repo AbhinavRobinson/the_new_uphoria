c058b943a218b0cf0869887c86c8d4c566d691c5
Abhinav Robinson <32090909+AbhinavRobinson@users.noreply.github.com>
2019-01-29 11:29:20 +0530

    Bug Fix : Menu

--- app/js/main.babel.js
+++ app/js/main.babel.js
@@ -1,6 +1,8 @@
 "use strict";
 
+// console.log("OK")
+
 var script = document.createElement('script');
-script.src = './src/js/jquery-3.3.1.min.js';
+script.src = './app/js/jquery-3.2.1.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
--- temp shift/app/js/main.babel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var script = document.createElement('script');
-script.src = './src/js/jquery-3.3.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
